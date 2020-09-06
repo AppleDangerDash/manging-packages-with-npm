f8e2e64603b8f715bb038143518c597a3f838a7b
Mike <haithemharaga@yahoo.co.uk>
2020-09-06 19:16:17 +0100

    adding a time

--- myApp.js
+++ myApp.js
@@ -56,7 +56,7 @@ app.get('/now',function(req, res, next){
     req.time = new Date().toString();
     next();
 }, function(req, res) {
-    res.send({time: req.time})
+    res.send({time: req.time })
 })
 
 /** 9)  Get input from client - Route parameters */
