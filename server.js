6b90c9a66e06a6c025c910a33c985cffe50d5118
Mike <haithemharaga@yahoo.co.uk>
2020-09-06 16:43:56 +0100

    add app.get

--- server.js
+++ server.js
@@ -58,6 +58,11 @@ if (!process.env.DISABLE_XORIGIN) {
 //   }  
 // })
 
+
+app.get("/",function(req,res){
+  res.send("Hello Express");
+})
+
 var port = process.env.PORT || 3000
 bGround.setupBackgroundApp(app, myApp, __dirname).listen(port, function () {
   bGround.log('Node.js listening on port' + port +"...");
