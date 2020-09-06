f39ce92496b1762f2935624f55305795d83fd3ba
Mike <haithemharaga@yahoo.co.uk>
2020-09-06 19:25:45 +0100

    niasd

--- myApp.js
+++ myApp.js
@@ -52,18 +52,18 @@ var jsonResponse = {"message":"Hello json"};
 
 
 /** 8) Chaining middleware. A Time server */
-app.get(
-    "/now",
-    (req, res, next) => {
-      req.time = new Date().toString();
-      next();
-    },
-    (req, res) => {
-      res.send({
-        time: req.time
-      });
-    }
-  );
+function getTheCurrentTimeString(){
+    return new Date().toString();
+}
+
+app.get("/now",function(req, res, next){
+    req.time = getTheCurrentTimeString();
+    next();
+}, function(req, res){
+    res.json({ time: req.time});
+})
+
+
 
 /** 9)  Get input from client - Route parameters */
 
